13c82d78484734bfe4db84925ab12468b348246d
Robert Preston <rob@Roberts-iMac.local>
2024-10-09 15:27:42 +0100

    Refactoring load.py with correct DB info and insert statement

--- src/load.py
+++ src/load.py
@@ -1,29 +1,28 @@
 import psycopg2
-from src.transform import transform_data
 
 def connect_to_db():
     # Replace with your connection details
-    conn = psycopg2.connect("dbname='yourdbname' user='youruser' password='yourpassword' host='localhost'")
+    conn = psycopg2.connect("dbname='pokecards_db' user='rob' password='PSQLCollege1!' host='localhost'")
     return conn
 
-def update_database(dataframe):
+def update_database(JSON):
     conn = connect_to_db()
     cursor = conn.cursor()
 
-    for index, row in dataframe.iterrows():
+    for card in JSON.iterrows():
         # Check if the card exists by both name and set
-        cursor.execute("SELECT * FROM pokecards WHERE name = %s AND card_set = %s", (row['name'], row['card_set']))
+        cursor.execute("SELECT * FROM stock WHERE name = %s AND card_set = %s", (row['name'], card['card_set']))
         existing_card = cursor.fetchone()
         
         if existing_card:
             # Update the existing card's stock
-            new_stock = existing_card[6] + row['stock']  # Adjust index as necessary
-            cursor.execute("UPDATE pokecards SET stock = %s WHERE name = %s AND card_set = %s", 
-                           (new_stock, row['name'], row['card_set']))
+            new_stock = existing_card[6] + card['quantity']  # Adjust index as necessary
+            cursor.execute("UPDATE stock SET quantity = %s WHERE name = %s AND card_set = %s",
+                           (new_stock, card['name'], card['card_set']))
         else:
             # Insert new card
-            cursor.execute("INSERT INTO pokecards (unique_stock_number, name, type, card_set, rarity, sale_price, stock) VALUES (%s, %s, %s, %s, %s, %s, %s)",
-                           (row['unique_stock_number'], row['name'], row['type'], row['card_set'], row['rarity'], row['sale_price'], row['stock']))
+            cursor.execute("INSERT INTO stock (name, type, card_set, rarity, sale_price, stock) VALUES (%s, %s, %s, %s, %s, %s, %s)",
+                           (card['name'], card['type'], card['card_set'], card['rarity'], card['sale_price'], card['stock']))
 
     conn.commit()
     cursor.close()
